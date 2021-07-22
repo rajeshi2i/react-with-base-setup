7ed559fbbd7ca751d83a46b039fb9b5bb5cf17e3
Rajasekar Venugopal <rajasekar.v@ideas2it.com>
2021-07-22 18:34:23 +0530

    Fix: App component's test case

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,9 +1,18 @@
 import React from 'react';
 import { render, screen } from '@testing-library/react';
-import App from './App';
+import { Provider } from 'react-redux';
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+import App from './App';
+import store from './store';
+
+test('renders header links', () => {
+  render(
+    <Provider store={store}>
+      <App />
+    </Provider>
+  );
+  const todoElement = screen.getByText(/TODO List/i);
+  expect(todoElement).toBeInTheDocument();
+  const homeElement = screen.getByText(/Home/i);
+  expect(homeElement).toBeInTheDocument();
 });
